0713cf13fb87618350487a610096b6e02a8ee85b
brijesh-u <brijeshupadhyay2001@gmail.com>
2022-02-04 14:02:53 +0530

    gmail navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SearchIcon from '@material-ui/icons/Search'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import MenuIcon from '@material-ui/icons/Menu'
+import NavContents from "./NavContents";
+import AppsIcon from '@material-ui/icons/Apps'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav_container">
+
+      <div className="nav_lef">
+        <div className='menu'>
+        <NavContents Icon={MenuIcon} title="" className="menu"/>
+        </div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2WdtWKYIx09KC9jN3L87mgntqtQwN1lMf0Q&usqp=CAU" alt="ggwp" className="img_first" />
+
+        <div className="search_box">
+          <SearchIcon/>
+          <input type="text" placeholder=""/>
+        </div>
+      </div>
+      <div className='nav_right'>
+        <NavContents Icon={AppsIcon} title=""/>
+        <NavContents Icon={NotificationsIcon} title="" />
+        <NavContents avatar title="" />
+      </div>
+      
     </div>
   );
 }
